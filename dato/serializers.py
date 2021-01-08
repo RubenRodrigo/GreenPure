5fa5784ca8106718f4814c1c461786efc796cc45
Eduardo Rodríguez <xeduardox54@hotmail.com>
2021-01-08 11:31:18 -0500

    Recepción de datos proporcionados por Arduino

--- dato/serializers.py
+++ dato/serializers.py
@@ -11,16 +11,18 @@ class DatosSerializer(serializers.ModelSerializer):
         """
         Create and return a new `Serie` instance, given the validated data.
         """
-        return Serie.objects.create(**validated_data)
+        return Datos.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         """
         Update and return an existing `Serie` instance, given the validated data.
         """
-        instance.Ubicacion = validated_data.get('Ubicacion', instance.Ubicacion)
+        instance.Humedad = validated_data.get('Humedad',instance.Humedad)
         instance.Temperatura = validated_data.get('Temperatura', instance.Temperatura)
         instance.Calor = validated_data.get('Calor', instance.Calor)
         instance.Concentracion = validated_data.get('Concentracion', instance.Concentracion)
+        instance.Latitud = validated_data.get('Latitud', instance.Latitud)
+        instance.Longitud = validated_data.get('Longitud', instance.Longitud)
         instance.SensorHumo = validated_data.get('SensorHumo', instance.SensorHumo)
         instance.SensorMetano = validated_data.get('SensorMetano', instance.SensorMetano)
         instance.fecha = validated_data.get('fecha', instance.fecha)
