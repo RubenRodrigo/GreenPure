f29ae73b1980a25cc15cfac29505cc1b875890c3
Eduardo Rodríguez <xeduardox54@hotmail.com>
2021-01-17 23:21:58 -0500

    Cálculo de calidad

--- dato/resumen.py
+++ dato/resumen.py
@@ -32,14 +32,46 @@ def obtenerDistrito(location, id):
             try:
                 distrito = location.raw['address']['county']
             except:
-                distrito = str(id) + "-No disponible"
+                try:
+                    distrito = location.raw['address']['region']
+                except:
+                    distrito = str(id) + "-No disponible"
     return distrito
 
 #Métodos de resumen
+def obtenerCalidad(dato):
+    calidad = 0
+    if dato.Temperatura <= 26 and dato.Temperatura >= 15:
+        calidad += 10
+    elif dato.Temperatura < 15 and dato.Temperatura >= 5:
+        calidad += 0
+    else:
+        calidad -= 20
+
+    if dato.Humedad <= 65 and dato.Humedad >= 55:
+        calidad += 10
+    elif dato.Humedad < 55 and dato.Humedad >= 40:
+        calidad += 0
+    else:
+        calidad -= 20
+    
+    if dato.SensorMetano:
+        calidad += 30
+    else:
+        calidad -= 30
+    
+    if dato.SensorHumo:
+        calidad += 20
+    else:
+        calidad -= 20
+    
+    return calidad
+
 def correccionOrientacionResumen(datosResumidos, paisesCiudades, distritos, datosDistrito):
     resumenNivel3 = []
     resumenNivel2 = []
     resumenFinal = []
+    calidades = []
     for item in datosResumidos:
         contador = 0
         for item2 in item.ubicaciones:
@@ -47,11 +79,13 @@ def correccionOrientacionResumen(datosResumidos, paisesCiudades, distritos, dato
                 contador2 = 0
                 for item3 in item2.datos:
                     if item2.distrito == datosDistrito[contador2]:
-                        resumenNivel3.append(item3)    
+                        resumenNivel3.append(item3)  
+                        calidades.append(item3.calidad)  
                     contador2 = contador2 +1
                 resumenNivel2.append(ElementoResumido(item2.id, item2.distrito, resumenNivel3))
                 resumenNivel3 = []
             contador = contador + 1
-        resumenFinal.append(DatoResumido(item.id, item.pais, item.ciudad, item.calidadAVG, resumenNivel2))
+        resumenFinal.append(DatoResumido(item.id, item.pais, item.ciudad, sum(calidades)/len(calidades), resumenNivel2))
         resumenNivel2 = []
+        calidades = []
     return resumenFinal
