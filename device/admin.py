e9d9356d497df4befe256ea0e913ea4bce47e1e2
Ruben Rodrigo <rodrigohde905@gmail.com>
2021-12-02 17:08:19 -0500

    FakeData

--- device/admin.py
+++ device/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
+from data.models import Data
 
 from device.models import Device
 
 # Register your models here.
 
 
+class DataInline(admin.TabularInline):
+    model = Data
+
+
 class DeviceAdmin(admin.ModelAdmin):
     readonly_fields = ('unique_id',)
+    inlines = [
+        DataInline,
+    ]
 
 
 admin.site.register(Device, DeviceAdmin)
