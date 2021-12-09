e6c012fa0eb04330a392a10018a9b7b097cc1af2
Ruben Rodrigo <rodrigohde905@gmail.com>
2021-12-08 21:58:47 -0500

    Docker Development and production

--- data/clases.py
+++ data/clases.py
@@ -1,4 +1,4 @@
-#Clases de objetos resumen
+# Clases de objetos resumen
 class DatoResumido:
     def __init__(self, id, pais, ciudad, calidadAVG, ubicaciones):
         self.id = id
@@ -6,11 +6,15 @@ class DatoResumido:
         self.ciudad = ciudad
         self.calidadAVG = calidadAVG
         self.ubicaciones = ubicaciones
+
+
 class ElementoResumido:
     def __init__(self, id, distrito, datos):
         self.id = id
         self.distrito = distrito
         self.datos = datos
+
+
 class CaracteristicasElemento:
     def __init__(self, latitud, longitud, calidad, fecha, hora, humedad, temperatura, calor, concentracion, sensorHumo, sensorMetano):
         self.latitud = latitud
@@ -24,23 +28,31 @@ class CaracteristicasElemento:
         self.concentracion = concentracion
         self.sensorHumo = sensorHumo
         self.sensorMetano = sensorMetano
-    
-#Clases para funcionalidad de datos enfocados
+
+# Clases para funcionalidad de datos enfocados
+
+
 class Ciudad:
     def __init__(self, idCiudad, nombre):
         self.idCiudad = idCiudad
         self.nombre = nombre
+
+
 class CiudadDistritos:
     def __init__(self, idCiudad, nombre, distritos):
         self.idCiudad = idCiudad
         self.nombre = nombre
         self.distritos = distritos
+
+
 class DistritoAuxiliar:
     def __init__(self, idDistrito, nombre, ciudadNombre, calidad):
         self.idDistrito = idDistrito
         self.nombre = nombre
         self.ciudadNombre = ciudadNombre
         self.calidad = calidad
+
+
 class DistritoEnfocado:
     def __init__(self, idDistrito, ciudad, distrito, humedad, temperatura, concentracion, hora, fecha, calidadAVG):
         self.idDistrito = idDistrito
@@ -52,6 +64,8 @@ class DistritoEnfocado:
         self.hora = hora
         self.fecha = fecha
         self.calidadAVG = calidadAVG
+
+
 class DistritoMapa:
     def __init__(self, idDistrito, ciudad, distrito, latitud, longitud, humedad, temperatura, concentracion, hora, fecha, calidadAVG):
         self.idDistrito = idDistrito
@@ -65,6 +79,8 @@ class DistritoMapa:
         self.hora = hora
         self.fecha = fecha
         self.calidadAVG = calidadAVG
+
+
 class Distrito:
     def __init__(self, idDistrito, nombre, ciudadNombre, calidadAVG, datos):
         self.idDistrito = idDistrito
@@ -73,7 +89,9 @@ class Distrito:
         self.calidadAVG = calidadAVG
         self.datos = datos
 
-#Clase para funcionalidad con Arduino
+# Clase para funcionalidad con Arduino
+
+
 class DatoCalidad:
     def __init__(self, id, humedad, temperatura, calor, concentracion, latitud, longitud, sensorHumo, sensorMetano, fecha, calidad):
         self.id = id
